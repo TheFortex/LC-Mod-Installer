ae9a1593a718069fc135894b94e24da044337d51
TheFortex <airtex.mc@gmail.com>
2023-12-18 23:59:53 +0200

    fixed auto-update functionality (again) (I tested it this time)

--- main.py
+++ main.py
@@ -4,6 +4,23 @@ from globals import __version
 
 # Check for updates
 
+UpdaterBatch = """
+@echo off
+title Updating LC Mod Installer...
+echo Killing process %1...
+taskkill /pid %1 /f > nul 2>&1
+:wait
+tasklist | find /i "installmods.exe" > nul 2>&1
+if errorlevel 1 (
+	move /Y installmods_new.exe installmods.exe
+	start installmods.exe
+	exit
+) else (
+	timeout /t 1 /nobreak > nul
+	goto wait
+)
+"""
+
 while True:
 	try:
 		print("Checking for updates...")
@@ -11,23 +28,21 @@ while True:
 		assert response.status_code == 200, f"Response code: {response.status_code}"
 		remote_version = int(response.json()["name"].split(" ")[1])
 
-		def Update(response):
-			download_url = response.json()["assets"][0]["browser_download_url"]
-			response = requests.get(download_url)
-			assert response.status_code == 200, f"Response code: {response.status_code}"
-			with open("installmods.exe", "wb") as file:
-				file.write(response.content)
-
-			os.system("installmods.exe")
-			sys.exit()
-
 		if remote_version > __version:
-			print("New version available, updating...")
+			print(f"New version available: {remote_version} (Current: {__version}) - The program will now update.")
 			os.system("pause")
 			while True:
 				try:
-					Update(response)
-					break
+					download_url = response.json()["assets"][0]["browser_download_url"]
+					response = requests.get(download_url)
+					assert response.status_code == 200, f"Response code: {response.status_code}"
+					with open("installmods_new.exe", "wb") as file:
+						file.write(response.content)
+
+					with open("update.bat", "w") as file:
+						file.write(UpdaterBatch)
+
+					os.system(f"start update.bat {os.getpid()}")
 				except Exception as e:
 					print("Failed to update:")
 					print(e)
@@ -44,6 +59,8 @@ while True:
 
 if os.path.exists(f"./TEMPDOWNLOAD.zip"): os.remove(f"./TEMPDOWNLOAD.zip")
 if os.path.exists(f"./TEMPEXTRACT"): shutil.rmtree("./TEMPEXTRACT")
+if os.path.exists(f"./installmods_new.exe"): os.remove(f"./installmods_new.exe")
+if os.path.exists(f"./update.bat"): os.remove(f"./update.bat")
 
 # Start
 
